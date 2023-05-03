e0ef2aa4208335ee3b03536854f99a3fb16af093
anjali251999 <132471579+anjali251999@users.noreply.github.com>
2023-05-03 20:04:31 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,20 @@
+public class Number{
+public static void main(String[] args) {
+
+int input = 0;// try input as 20 and -4
+if(input> 0) {
+System.out.println("Number is positive");
+}
+
+else if (input < 0) {
+System.out.println("Number is negative");
+
+}
+else {
+System.out.println("Number is zero");
+}
+
+
+}
+}
+
---
+++ variable.java
@@ -0,0 +1,15 @@
+public class VariableDemo{
+	public static void main(String[] args) {
+	int numberOfParticipants = 75;
+	String nameOfCourse = "Java Fsd";
+	int durationInMins = 120;
+	System.out.println("numberofparticipants is " + numberofparticipants);
+	System.out.println(nameofcourse);
+	int num1 = (10 + 2) * 2;
+	int num2 = 10 + (5 + 2);
+	System.out.println("num1 is" +num1);
+	System.out.println("num2 is" +num2);
+
+	}
+} 
+
